076d313964d4c49b1011d98d0e90b9c7bd0a41ad
NickKelloggDDD <nicholas.mcdermott@kellogg.northwestern.edu>
2021-06-04 15:02:46 -0500

    nick commit

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,16 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+//Nick's firebase info
+// Nick/Group firebase info
+apiKey: "AIzaSyC2fn9VA6Y1Bfplu43HUNDb6lV1cUq5AuU",
+authDomain: "final-49027.firebaseapp.com",
+projectId: "final-49027",
+storageBucket: "final-49027.appspot.com",
+messagingSenderId: "33461789910",
+appId: "1:33461789910:web:bd4b77badbe7a2aa3b9577"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
