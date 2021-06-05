825771f55d75cb5210cbf2b8024dfc09c919ca12
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-06-05 18:11:13 -0500

    0611

--- netlify/functions/fitnessproviders.js
+++ netlify/functions/fitnessproviders.js
@@ -1,14 +1,14 @@
 // allows us to use firebase
 let firebase = require(`./firebase`)
 
-// /.netlify/functions/courses?courseNumber=KIEI-451
+// /.netlify/functions/gyms?type=Boxing
 exports.handler = async function(event) {
 
-  // get the activity type being requested
+  // get the provider name and type
   let activity = event.queryStringParameters.activity
 
   // establish a connection to firebase in memory
-  let db = firebase.firestore() 
+  let db = firebase.firestore()
 
   // ask Firebase for the course that corresponds to the course number, wait for the response
   let courseQuery = await db.collection('courses').where(`courseNumber`, `==`, courseNumber).get()
