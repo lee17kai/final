6f75d1c445a0007e346fbf2569dc4ef00f6eb3d0
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-06-06 14:14:38 -0500

    0214

---
+++ netlify/functions/create_user.js
@@ -0,0 +1,23 @@
+// Goal: Provide a function to create a new user in Firebase
+// allows us to use firebase
+let firebase = require(`./firebase`)
+
+exports.handler = async function(event) {
+  // get the querystring parameters and store in memory
+  let userName = event.queryStringParameters.userName
+  let fitnessProvider = event.queryStringParameters.fitnessProvider
+  let reservationId = event.queryStringParameters.reservationId
+
+  // establish a connection to firebase in memory
+  let db = firebase.firestore()
+
+  // create a new post, wait for it to return
+  await db.collection('reservations').add({
+    customerId: ,
+    fitnessProviderId: ,
+    time: ,
+  })
+  return {
+    statusCode: 200
+  }
+}
