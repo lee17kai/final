10a1e45649d79d2fb29e68b8d049d5a71f195927
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-06-06 10:53:47 -0500

    1053

--- netlify/functions/fitnessproviders.js
+++ netlify/functions/fitnessproviders.js
@@ -1,10 +1,10 @@
 // allows us to use firebase
 let firebase = require(`./firebase`)
 
-// /.netlify/functions/gyms?type=Boxing
+// /.netlify/functions/fitnessProviders?activity=Boxing
 exports.handler = async function(event) {
 
-  // get the provider name and type
+  // get the provider activity
   let activity = event.queryStringParameters.activity
 
   // establish a connection to firebase in memory
@@ -18,48 +18,113 @@ exports.handler = async function(event) {
 
   // create an object with the course data to hold the return value from our lambda
   let returnValue = {
-    activity: providerData.activity,
+    activity: activity,
     fitnessProviders: []
   }
+  //create counters to track total number of providers found and sum of ratings
+  let countProviders = 0
+  let sumProviderRatings = 0
 
-  //loop through the providers
+  // loop through the providers
   for(let providerIndex = 0; providerIndex < providers.length; providerIndex++){
 
-    //get the document id of the provider
+    // get the document id of the provider
     let fitnessProviderId = providers[providerIndex].id
 
-    //get the data from the provider
-    let providerData = providers[providerIndex].data()
+    // get the data from the provider
+    let providerData = fitnessProviderId.data()
 
     // create an object to be added to the return value of our lambda
     let providerObject = {
-      providerName: "",
-      activity: "",
-      address: "",
+      providerName: providerData.providerName,
+      activity: providerData.activity,
+      address: providerData.address,
       avgRating: "",
-      description: "",
-      level: "",
+      description: providerData.description,
+      level: providerData.level,
       reservations: [],
     }
 
-    // add the details to the provider object
-    providerObject.providerName = providerData.providerName
-    providerObject.address = providerData.address
-    providerObject.activity = providerData.activity
-    providerObject.description = providerData.description
-    providerObject.level = providerData.level
-
     //now we gotta create the array of reservation objects
-    // ask Firebase for the sections corresponding to the Document ID of the reservation, wait for the response
+    // ask Firebase for the reservations corresponding to the doc id of provider, wait for the response
     let reservationsQuery = await db.collection('reservations').where(`fitnessProviderId`, `==`, fitnessProviderId).get()
 
     // get the documents from the query .docs turns courseQuery into array
     let reservations = reservationsQuery.docs
 
-    for(reservationIndex = 0; reservationIndex < reservations.length; reservationIndex++)
-    
-    // add the section Object to the return value
-    returnValue.sections.push(providerObject)
+    for(reservationIndex = 0; reservationIndex < reservations.length; reservationIndex++){
+
+      //get the document ID of the section
+      let reservationId = reservations[reservationIndex].id
+
+      //get the data from the section
+      reservationData = reservationId.data()
+
+      //create an object to be added to the return value
+      let reservationObject = {
+        providerName: "",
+        time: reservationData.time,
+        customerList: []
+      }
+
+      //ask firebase for the provider for this reservation
+      let reservationProviderQuery = await db.collection(`fitnessProviders`).doc(reservationData.fitnessProviderId).get()
+
+      //get the data from the returned document
+      let reservationProvider = reservationProviderQuery.data()
+
+      // add the provider name from the fitness provider to the reservation object
+      reservationObject.providerName = reservationProvider.providerName
+
+      //ok now another loop for the customers array within each reservation!
+      // ask Firebase for the list of reservations that match our current provider and current reservation time
+      let reservationTimeQuery = await db.collection('reservations').where(`fitnessProviderId`, `==`, fitnessProviderId).where(`time`, `==`, reservationData.time).get()
+
+      // turn this query into an array of reservations that represents a list of customers that signed up for each rservation
+      let customers = reservationTimeQuery.docs
+
+      //loop through this array, turning the array of reservations into an array of customer objects
+      for(customerIndex = 0; customerIndex < customers.length; customerIndex++){
+
+        //get document ID of the reservation
+        let reservationId2 = customers[customerIndex].id
+
+        // get the data for this reservation
+        let reservationData2 = reservationId2.data()
+
+        // get the customer data for this reservation
+        let customerId = reservationData2.customerId
+
+        //ask firebase to go through the users and find the customer 
+        let customersQuery = await db.collection(`users`).where(`customerId`, `==`, customerId).get()
+
+        // get first document from this array
+        let customer = customersQuery.docs[0]
+
+        //get the data for this customer
+        let customerData = customer.data()
+
+        // create a customer object
+        let customerObject = {
+          name: customerData.userName,
+          email: customerData.userEmail,
+          password: customerData.userPassword
+        }
+
+        //add customer object to the reservation object
+        reservationObject.customerList.push(customerObject)
+      }
+
+      //add reservation object to the provider object
+      providerObject.reservations.push(reservationObject)
+
+    }
+
+    //add to counter
+    countProviders = countProviders + 1
+
+    // add the provider Object to the return value
+    returnValue.fitnessProviders.push(providerObject)
   }
 
   // return the standard response 
